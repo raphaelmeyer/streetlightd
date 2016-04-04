ff31fe48fd8145f1dbcd81bd3410ef60ffd079c9
Simon Egli <simon.egli@bbv.ch>
2016-04-04 14:48:05 +0200

    Updating Session Interface
    As discussed with Urs
    
    Signed-off-by: Simon Egli <simon.egli@bbv.ch>

--- source/presentation/Session.h
+++ source/presentation/Session.h
@@ -17,14 +17,15 @@ public:
   
   //TODO Following are method stubs, they may be altered to fit our needs
   
-  virtual bool setUp() = 0;
-  virtual bool connect() = 0;
+  virtual void setUp() = 0;
+  virtual void connect() = 0;
 
 
   //TODO do not use a string (in the case somebody wants to send binary data)
   virtual void send(const std::string &message) = 0;
-  virtual bool subscribe(std::string target) = 0;
-  virtual bool close() = 0;
+  virtual void subscribe(const std::string& target) = 0;
+  virtual void close() = 0;
+  virtual void setMessageCallback(std::function<void(const std::string&)> function) = 0;
 
 };
 
