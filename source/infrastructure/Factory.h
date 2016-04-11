f1bd084055e843595cb25a9a4a40a46cd881b1a9
Urs Fässler <urs.fassler@bbv.ch>
2016-04-11 10:40:57 +0200

    add factory

---
+++ source/infrastructure/Factory.h
@@ -0,0 +1,55 @@
+/*
+ * (C) Copyright 2016
+ * Urs Fässler, bbv Software Services, http://bbv.ch
+ *
+ * SPDX-License-Identifier:	GPL-3.0+
+ */
+
+#ifndef FACTORY_H
+#define FACTORY_H
+
+#include <string>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <functional>
+#include <algorithm>
+
+template<class T>
+class Factory
+{
+public:
+  typedef std::function<T*()> Worker;
+
+  void add(const std::string &product)
+  {
+    worker[product] = []{return nullptr;};
+  }
+
+  void add(const std::string &product, Worker worker)
+  {
+    this->worker[product] = worker;
+  }
+
+  std::set<std::string> workers() const
+  {
+    std::set<std::string> result{};
+    std::transform(worker.begin(), worker.end(), std::inserter(result, result.end()), [](const auto &pair){return pair.first;});
+    return result;
+  }
+
+  T* produce(const std::string &product) const
+  {
+    const auto pos = worker.find(product);
+    if (pos == worker.end()) {
+      throw std::invalid_argument("no worker for product: " + product);
+    }
+    return pos->second();
+  }
+
+private:
+  std::map<std::string, Worker> worker;
+
+};
+
+#endif
