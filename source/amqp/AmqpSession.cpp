46dd61b25d3c441303eff9fa1556eefe29b73beb
Urs Fässler <urs.fassler@bbv.ch>
2016-04-13 11:56:00 +0200

    remove includes from public interface

--- source/amqp/AmqpSession.cpp
+++ source/amqp/AmqpSession.cpp
@@ -1,6 +1,69 @@
 #include "AmqpSession.h"
 
+extern "C"
+{
+#include "platform.h"
+#include "iothub_client.h"
+#include "iothub_message.h"
+#include "threadapi.h"
+#include "crt_abstractions.h"
+#include "iothubtransportamqp.h"
+}
+
 #include <exception>
+
+
+
+/** Internal message callback which is executed when a message is received
+       * @param message a handle to the message received
+       * @param userContextCallback this will hold a pointer to a AMQPSession
+       * @return IOTHUBMESSAGE_ACCEPTED if everything went well
+       */
+static IOTHUBMESSAGE_DISPOSITION_RESULT internalMessageCallback(IOTHUB_MESSAGE_HANDLE message, void* userContextCallback) {
+  printf("Received a message");
+  if(userContextCallback == nullptr)
+    throw std::runtime_error("Error in internalMessageCallback, no callback function");
+
+  Session::Callback *callback = reinterpret_cast<Session::Callback*>(userContextCallback);
+
+  const unsigned char* buffer = NULL;
+  size_t size = 0;
+
+  IOTHUBMESSAGE_CONTENT_TYPE contentType = IoTHubMessage_GetContentType(message);
+
+  if (contentType == IOTHUBMESSAGE_BYTEARRAY)
+  {
+    if (IoTHubMessage_GetByteArray(message, &buffer, &size) == IOTHUB_MESSAGE_OK)
+    {
+      //(void)printf("Received Message  with BINARY Data: <<<%.*s>>> & Size=%d\r\n",  (int)size, buffer, (int)size);
+      (*callback)(std::string(reinterpret_cast<const char*>(buffer), size));
+    }
+    else
+    {
+      throw std::runtime_error("Failed getting the BINARY body of the message received.\r\n");
+    }
+  }
+  else if (contentType == IOTHUBMESSAGE_STRING)
+  {
+    //NOTE type unsafe reinterpretation casts!
+    if ((buffer = reinterpret_cast<const unsigned char*>(IoTHubMessage_GetString(message))) != NULL && (size = strlen(reinterpret_cast<const char*>(buffer))) > 0)
+    {
+      //(void)printf("Received Message with STRING Data: <<<%.*s>>> & Size=%d\r\n",(int)size, buffer, (int)size);
+      (*callback)(std::string(reinterpret_cast<const char*>(buffer), size));
+    }
+    else
+    {
+      throw std::runtime_error("Failed getting the STRING body of the message received.\r\n");
+    }
+  }
+  else
+  {
+    //(void)printf("Failed getting the body of the message received (type %i).\r\n", contentType);
+    throw std::runtime_error("Failed getting the STRING body of the message received.\r\n");
+  }
+
+  return IOTHUBMESSAGE_ACCEPTED;
+}
 
 static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT, void*)
 {
@@ -35,7 +98,7 @@ void AmqpSession::connect()
   }
 
   /* Setting Message call back, so we can receive Commands. Give this pointer to callback*/
-  if (IoTHubClient_SetMessageCallback(iotHubClientHandle_, (&AmqpSession::internalMessageCallback), this) != IOTHUB_CLIENT_OK)
+  if (IoTHubClient_SetMessageCallback((IOTHUB_CLIENT_HANDLE)iotHubClientHandle_, internalMessageCallback, &receivedFunction_) != IOTHUB_CLIENT_OK)
   {
     throw std::runtime_error("ERROR: IoTHubClient_SetMessageCallback..........FAILED!\r\n");
   }
@@ -50,7 +113,7 @@ void AmqpSession::send(const std::string& message)
     throw std::runtime_error("Couldn't create MessageHandle");
   }
 
-  if (IoTHubClient_SendEventAsync(iotHubClientHandle_, messageHandle, SendConfirmationCallback, this) != IOTHUB_CLIENT_OK)
+  if (IoTHubClient_SendEventAsync((IOTHUB_CLIENT_HANDLE)iotHubClientHandle_, messageHandle, SendConfirmationCallback, this) != IOTHUB_CLIENT_OK)
   {
     throw std::runtime_error("ERROR: IoTHubClient_SendEventAsync..........FAILED!\r\n");
   }
@@ -66,52 +129,8 @@ void AmqpSession::setMessageCallback(Callback function) {
 
 void AmqpSession::close()
 {
-  IoTHubClient_Destroy(iotHubClientHandle_);
+  IoTHubClient_Destroy((IOTHUB_CLIENT_HANDLE)iotHubClientHandle_);
+  iotHubClientHandle_ = nullptr;
   platform_deinit();
 }
 
-IOTHUBMESSAGE_DISPOSITION_RESULT AmqpSession::internalMessageCallback(IOTHUB_MESSAGE_HANDLE message, void* userContextCallback) {
-  printf("Received a message");
-  if(userContextCallback == nullptr)
-    throw std::runtime_error("Error in internalMessageCallback, no \"this\" pointer");
-
-  AmqpSession* instance = reinterpret_cast<AmqpSession*>(userContextCallback);
-
-  const unsigned char* buffer = NULL;
-  size_t size = 0;
-
-  IOTHUBMESSAGE_CONTENT_TYPE contentType = IoTHubMessage_GetContentType(message);
-
-  if (contentType == IOTHUBMESSAGE_BYTEARRAY)
-  {
-    if (IoTHubMessage_GetByteArray(message, &buffer, &size) == IOTHUB_MESSAGE_OK)
-    {
-      //(void)printf("Received Message  with BINARY Data: <<<%.*s>>> & Size=%d\r\n",  (int)size, buffer, (int)size);
-      instance->receivedFunction_(std::string(reinterpret_cast<const char*>(buffer), size));
-    }
-    else
-    {
-      throw std::runtime_error("Failed getting the BINARY body of the message received.\r\n");
-    }
-  }
-  else if (contentType == IOTHUBMESSAGE_STRING)
-  {
-    //NOTE type unsafe reinterpretation casts!
-    if ((buffer = reinterpret_cast<const unsigned char*>(IoTHubMessage_GetString(message))) != NULL && (size = strlen(reinterpret_cast<const char*>(buffer))) > 0)
-    {
-      //(void)printf("Received Message with STRING Data: <<<%.*s>>> & Size=%d\r\n",(int)size, buffer, (int)size);
-      instance->receivedFunction_(std::string(reinterpret_cast<const char*>(buffer), size));
-    }
-    else
-    {
-      throw std::runtime_error("Failed getting the STRING body of the message received.\r\n");
-    }
-  }
-  else
-  {
-    //(void)printf("Failed getting the body of the message received (type %i).\r\n", contentType);
-    throw std::runtime_error("Failed getting the STRING body of the message received.\r\n");
-  }
-
-  return IOTHUBMESSAGE_ACCEPTED;
-}
--- source/amqp/AmqpSession.h
+++ source/amqp/AmqpSession.h
@@ -1,15 +1,5 @@
 #include <protocolstack/session/Session.h>
 
-extern "C"
-{
-#include "platform.h"
-#include "iothub_client.h"
-#include "iothub_message.h"
-#include "threadapi.h"
-#include "crt_abstractions.h"
-#include "iothubtransportamqp.h"
-}
-
 #include <string>
 #include <functional>
 
@@ -24,14 +14,8 @@ public:
   void close() override;
   void setMessageCallback(Callback function) override;
 
-  /** Internal message callback which is executed when a message is received
-         * @param message a handle to the message received
-         * @param userContextCallback this will hold a pointer to a AMQPSession
-         * @return IOTHUBMESSAGE_ACCEPTED if everything went well
-         */
-  static IOTHUBMESSAGE_DISPOSITION_RESULT internalMessageCallback(IOTHUB_MESSAGE_HANDLE message, void* userContextCallback);
-
+private:
   std::string connectionString{};
-  IOTHUB_CLIENT_HANDLE iotHubClientHandle_; ///!< Our internal connection handle
+  void* iotHubClientHandle_{nullptr}; ///!< Our internal connection handle
   Callback receivedFunction_; ///!< Function to execute on receiving
 };
